5f92281fff851a52137050691188ecbae24019b7
cVitorAndrade <vitorandrade1804@gmail.com>
2022-12-14 20:40:35 -0300

    structuring modal data

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight') 
 const inputHeight = document.querySelector('#height')
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+
+const Modal = {
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
 
 form.onsubmit = function (e) {
     e.preventDefault()
     const weight = inputWeight.value
     const height = inputHeight.value 
 
+    const showAlertError = notNumber(weight) || notNumber(height)
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    modalWrapper.classList.add('open')
-    modalMessage.innerText = message
+    Modal.message.innerText = message
+    Modal.open()
 }
 
-    modalBtnClose.onclick = () => modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => {
+    Modal.close()
+}
+
+function notNumber(value) {
+    return isNaN(value) || value == ""
+}
 
 function IMC(weight, height) {
     return (weight / ((height / 100) **2)).toFixed(2)
